d588077d42a2a13f0029e31dced654e41b335234
Connor <106984058+MrCPJT@users.noreply.github.com>
2022-09-23 23:46:57 +0100

    Commit

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ import spotipy.oauth2
 # print(os.getenv('SPOTIPY_REDIRECT_URI'))
 
 spotify = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
-scope = 'playlist-read-private'
+scope = 'user-top-read'
 sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=scope))
-user = sp.user('connor.tynan')
-results = sp.current_user_playlists(limit=50)
-for i, item in enumerate(results['items']):
-    print("%d %s" % (i, item['name']))
-#     print(i, j)
 
+ranges = ['short_term', 'medium_term', 'long_term']
 
-# birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-
+for sp_range in ranges:
+    print("range: ", range)
+    results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
+    for i, item in enumerate(results['items']):
+        # print(i, item)
+        print(i, item['name'], '//', item['artists'][0]['name'], '//', item['artists'][0]['type'], '//', item['type'])
+    print()
