d2101079a3c23c831f332adfb590b43f31050d5b
Connor <106984058+MrCPJT@users.noreply.github.com>
2022-09-24 00:12:26 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from unittest import result
+# from unittest import result
 import spotipy
 import spotipy.oauth2
 # import os
@@ -14,9 +14,12 @@ sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=scope))
 ranges = ['short_term', 'medium_term', 'long_term']
 
 for sp_range in ranges:
-    print("range: ", range)
-    results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
+    print("-----------------------------------", "\n")
+    results = sp.current_user_top_tracks(time_range=sp_range, limit=10)
     for i, item in enumerate(results['items']):
         # print(i, item)
-        print(i, item['name'], '//', item['artists'][0]['name'], '//', item['artists'][0]['type'], '//', item['type'])
+        # print(i, item['name'], '//', item['artists'][0]['name'], '//', item['artists'][0]['type'], '//', item['type'])
+        if i == 0: print("----- Most Popular -----")
+        print((int(i) + 1), item['name'], '-', item['artists'][0]['name'])
+        if i == 9: print("----- Least Popular -----")
     print()
